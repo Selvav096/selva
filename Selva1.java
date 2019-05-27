549501d489d6a1b64fdb6ad001f49f8b08b39342
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-05-27 18:09:30 +0530

    Create Selva1.java

---
+++ Selva1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner (System.in);
+int a=s.nextInt();
+if(a>0)
+System.ouy.print("Positive");
+else if(a<0)
+System.out.print("Negative");
+else
+System.out.print("Zero");
+}
+}
